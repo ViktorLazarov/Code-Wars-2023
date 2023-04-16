3b3889ecb5658fa0f270730d4d273b67d3f4f8b3
viktor lazarov <viktorlazarov@abv.bg>
2023-04-16 13:11:33 +0200

    finished SumOfTwoLowestIntegers

--- sumOfTwoLowestIntegers.js
+++ sumOfTwoLowestIntegers.js
@@ -1,5 +1,10 @@
 function solve(input) {
+    let sorted = input.sort((a,b) => a - b)
+    let a = sorted[0];
+    let b = sorted[1];
+    return a + b;
 
 }
 
 solve([19, 5, 42, 2, 77])
+solve([5, 8, 12, 19, 22])
